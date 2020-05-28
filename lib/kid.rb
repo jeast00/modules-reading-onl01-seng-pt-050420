523b0f7899d82009b60d5e5bfe0714a13d478ae1
Jonathan East <jonathan_east@ymail.com>
2020-05-28 21:31:06 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+
+  # Declaring single modules
+  # include Dance # - 'include' is for instance methods
+  # extend MetaDancing # - 'extend' is for class methods
+
+  # Declaring nested modules
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
